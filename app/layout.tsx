6931c37bdf094b83df617a8964b2a600e2ecfdc5
Taran Glasson <taranglasson@gmail.com>
2025-05-07 17:20:10 +0100

    Stop root padding being applied twice on homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,16 @@ export default function RootLayout({
 			</head>
 			<body
 				className="antialiased prose mx-auto font-sans bg-[#FAF9F6] prose-h1:text-2xl prose-h3:text-base
-					px-4 py-6 
-					sm:px-8 sm:py-8
-					md:px-16 md:py-10
-					lg:px-56 lg:py-10
+					px-4
+					sm:px-8
+					md:px-16
+					lg:px-56
 					max-w-5xl"
 				suppressHydrationWarning
 			>
-				{children}
+				<div className="py-6 sm:py-8 md:py-10 lg:py-10">
+					{children}
+				</div>
 			</body>
 		</html>
 	);
